511e7d15786a1e8ffa152732a56c7d20bd3ce3f0
Phillip Choi <jaeja-phil@gmail.com>
2020-08-24 16:43:59 +0900

    add drawer navigator icon and openDrawer function

--- routes/Drawer.js
+++ routes/Drawer.js
@@ -2,7 +2,7 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 import HomeStack from './HomeStack';
-import AboutStack from './aboutStack';
+import AboutStack from './AboutStack';
 
 const RootDrawerNavigator = createDrawerNavigator({
   Home: {
