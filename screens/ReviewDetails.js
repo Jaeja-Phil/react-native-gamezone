55c9b2f279b7d9e5413409a1caa81505d9392fd4
Phillip Choi <jaeja-phil@gmail.com>
2020-08-24 22:34:47 +0900

    add imagebackground to header

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { globalStyles, images } from '../styles/Global';
 import Card from '../shared/Card';
 
 export default function ReviewDetails({ navigation }) {
+  const rating = navigation.getParam('rating');
+
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Card>
         <Text>{navigation.getParam('title')}</Text>
         <Text>{navigation.getParam('body')}</Text>
-        <Text>{navigation.getParam('rating')}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone Rating: </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 24,
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
   },
 });
